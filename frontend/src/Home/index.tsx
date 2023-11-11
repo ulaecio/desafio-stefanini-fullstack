32e91674c227f30e6ca1d45b6ed2cb7c214eb4c7
ulaecio <ulaecio@live.com>
2023-11-11 15:48:22 -0300

    Add formatted and válid inputs to name and phone

--- frontend/src/Home/index.tsx
+++ frontend/src/Home/index.tsx
@@ -1,82 +1,125 @@
-import Header from 'Header';
-import './styles.css'
-import { useState } from 'react';
+import Header from "Header";
+import "./styles.css";
+import { useState } from "react";
 
 const Home = () => {
-  const [nome, setNome] = useState('');
-  const [telefone, setTelefone] = useState('');
+  const [nome, setNome] = useState("");
+  const [telefone, setTelefone] = useState("");
   const [modalVisible, setModalVisible] = useState(false);
+  const [nomeValido, setNomeValido] = useState(true);
+  const [telefoneValido, setTelefoneValido] = useState(true);
 
-  const validarNome = () => {
-    // Implementa a lógica de validação para o nome (tamanho e tipo de caractere)
-    // Retorna true se válido, false caso contrário
-    return nome.length <= 30 && /^[a-zA-Z]+$/.test(nome);
+  const validarNome = (value: any) => {
+    return value.length <= 30 && /^([a-zA-Z]+\s){0,4}[a-zA-Z]+$/.test(value);
   };
 
-  const validarTelefone = () => {
-    // Implementa a lógica de validação para o telefone (tamanho e tipo de caractere)
-    // Retorna true se válido, false caso contrário
-    return telefone.length === 10 && /^\d+$/.test(telefone);
+  const formatarTelefone = (input: string) => {
+    const desformatado = input.replace(/\D/g, "");
+    const formatar = desformatado.match(/^(\d{2})(\d{4})(\d{4})$/);
+
+    if (formatar) {
+      return `(${formatar[1]}) ${formatar[2]}-${formatar[3]}`;
+    }
+    return input;
+  };
+
+  const handleTelefoneChange = (e: { target: { value: string; }; }) => {
+    const telefoneFormatado = formatarTelefone(e.target.value);
+
+    if (telefoneFormatado) {
+      setTelefoneValido(true);
+      setTelefone(telefoneFormatado);
+    } else {
+      setTelefoneValido(false);
+    }
   };
 
   const handleSalvar = () => {
-    if (validarNome()) {
-      // Exibe os valores em uma janela modal ou tabela (neste exemplo, será uma janela modal)
-    } else {
-      alert('Preencha o nome com até 30 caracteres.');
+    const nomeValido = validarNome(nome);
+    const telefoneValido = telefone.length === 14;
+
+    setNomeValido(nomeValido);
+    setTelefoneValido(telefoneValido);
+
+    if (nomeValido && telefoneValido) {
+      setModalVisible(true);
     }
-    if (validarTelefone()) {
-        // Exibe os valores em uma janela modal ou tabela (neste caso, será uma usado janela modal)
-        setModalVisible(true);
-      } else {
-        alert('Preencha o telefone com 10 digitos numéricos');
-      }
   };
 
   const handleLimpar = () => {
-    setNome('');
-    setTelefone('');
+    setNome("");
+    setTelefone("");
+    setNomeValido(true);
+    setTelefoneValido(true);
   };
 
   return (
     <>
-    <Header/>
-    <div className='stefanini-container stefanini-card'>
-      <h1 className='stefanini-cadastro-title'>Cadastro de Clientes</h1>
-      <label>
-        Nome:
-        <input 
-        className="stefanini-form-control"
-          type="text"
-          value={nome}
-          onChange={(e) => setNome(e.target.value)}
-        />
-      </label>
-      <br />
-      <label>
-        Telefone:
-        <input className="stefanini-form-control"
-          type="tel"
-          value={telefone}
-          onChange={(e) => setTelefone(e.target.value)}
-        />
-      </label>
-      <br />
-      <button className='home-btn' onClick={handleSalvar}>Salvar</button>
-      <button className='home-btn' onClick={handleLimpar}>Limpar</button>
-
-      {modalVisible && (
-        <div className='stefanini-container'>
-          <h2 className='stefanini-title-response'>Valores do Formulário:</h2>
-          <p>Nome: {nome}</p>
-          <p>Telefone: {telefone}</p>
-          <button className='button-fechar' onClick={() => setModalVisible(false)}>Fechar</button>
+      <Header />
+      <div className="stefanini-container stefanini-card">
+        <div className="stefanini-cadastro-input">
+          <h1 className="stefanini-cadastro-title">Cadastro de Clientes</h1>
+          <label>
+            Nome:
+            <input
+              className={`stefanini-form-control ${
+                nomeValido ? "" : "invalido"
+              }`}
+              type="text"
+              value={nome}
+              onChange={(e) => {
+                const value = e.target.value;
+                setNome(value);
+                setNomeValido(validarNome(value));
+              }}
+            />
+          </label>
+          {!nomeValido && (
+            <p className="mensagem-erro ">
+              Preencha o nome corretamente sem espaço final e com 30 caracteres.
+            </p>
+          )}
+          <br />
+          <label>
+            Telefone:
+            <input
+              className={`stefanini-form-control ${
+                telefoneValido ? "" : "invalido"
+              }`}
+              type="tel"
+              value={telefone}
+              onChange={handleTelefoneChange}
+            />
+          </label>
+          {!telefoneValido && (
+            <p className="mensagem-erro">
+              Preencha o telefone no formato (DD) DDDD-DDDD com 10 dígitos.
+            </p>
+          )}
+          <br />
+          <button className="home-btn" onClick={handleSalvar}>
+            Salvar
+          </button>
+          <button className="home-btn" onClick={handleLimpar}>
+            Limpar
+          </button>
         </div>
-      )}
-    </div>
-
+        {modalVisible && (
+          <div>
+            <h2 className="stefanini-title-response">Valores do Formulário:</h2>
+            <p>Nome: {nome}</p>
+            <p>Telefone: {telefone}</p>
+            <button
+              className="button-fechar"
+              onClick={() => setModalVisible(false)}
+            >
+              Fechar
+            </button>
+          </div>
+        )}
+      </div>
     </>
   );
 };
 
-export default Home;
+export default Home
